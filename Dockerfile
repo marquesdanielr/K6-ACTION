8a0ac3df0d113b60e9461edccab6a7bf4b8fd229
marquesdanielr <82175768+marquesdanielr@users.noreply.github.com>
2024-02-06 14:17:22 -0300

    teste

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM grafana/k6:latest
 
 USER 0
+
 COPY entrypoint.sh /entrypoint.sh
+chmod 777 *
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# `$*` expands the `args` supplied in an `array` individually 
+# `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
 export K6_CLOUD_TOKEN=$INPUT_TOKEN
 export K6_COMMAND=$([[ $INPUT_CLOUD == "true" ]] && echo 'cloud' || echo 'run')
